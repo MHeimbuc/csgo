99e7529fc4fb855d10410d4a181a46fcf09d6443
Markus Heimbuchner <MHeimbuc@users.noreply.github.com>
2021-01-03 03:49:06 +0100

    Update start.sh

--- containerfs/start.sh
+++ containerfs/start.sh
@@ -64,6 +64,16 @@ tv_relayvoice 1
 tv_snapshotrate 64
 tv_timeout 60
 tv_transmitall 1
+mp_match_end_restart 0
+mp_freezetime 0
+mp_autokick 0
+sm_mapvote_extend 999
+sm_mapvote_start 5
+sm_mapvote_include 20
+sm_mapvote_novote 0
+mp_roundtime_defuse 60
+mp_roundtime_hostage 60
+mp_roundtime 60
 writeid
 writeip
 SERVERCFG
